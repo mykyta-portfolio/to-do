399110ed7d3b367fe073fc6fe41c9dd374fc4e22
mykyta-karmannyi <nefechofgennadiy@gmail.com>
2025-11-18 01:23:55 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import videobg from "./videobg.mp4"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//localStorage.removeItem("data")
+	let data = JSON.parse(localStorage.getItem("data"))
+	if (data === null) data = []
+	const [items, setItems] = React.useState(data)
+	const inputEl = React.useRef(null);
+
+	const handleOnAdd = () => {
+		console.log("add done")
+		setItems([...items, inputEl.current.value])
+		localStorage.setItem("data", JSON.stringify(items))
+	}
+
+	const remove = (index) => {
+		console.log("remove done")
+		const tempItems = [...items];
+		tempItems.splice(index, 1);
+		setItems(tempItems)
+
+	}
+
+	React.useEffect(() => {
+		console.log("use effect")
+		const temp = JSON.stringify(items)
+		localStorage.setItem("data", temp)
+	}, [items])
+
+
+	return (
+		<>
+
+			<video autoPlay muted loop id="myVideo">
+				<source src={videobg} type="video/mp4" />
+			</video>
+			<div className="todo">
+				<h1>Список важливих завдань</h1>
+				<div className="form">
+					<input ref={inputEl} type="text" />
+					<button onClick={handleOnAdd}>Додати</button>
+				</div>
+				<ul>
+					{items.map((item, index) => (
+						<li>
+							<span>
+								{item}
+							</span>
+							<span class="delete" onClick={() => { remove(index) }}>
+								<i class="fa fa-trash"></i>
+							</span>
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default App;
